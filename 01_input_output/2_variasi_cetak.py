c33be8d7ca5a9edca9ce30879373e2d7da9a9e00
Fahri Firdausillah <elfaatta@gmail.com>
2022-11-12 05:28:38 +0700

    reset

--- 01_input_output/2_variasi_cetak.py
+++ 01_input_output/2_variasi_cetak.py
@@ -2,11 +2,8 @@
 
 # beginilah escaping character
 
-
 # default print diakhiri \n dan dipisah dengan " " (spasi)
 
-
 # perubahan akhiran bisa ditambahkan parameter "end"
-
 
 # karakter pemisah bisa didefinisikan dengan parameter "sep"
